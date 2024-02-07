3eba1ffcdbb882fd5d198899c3b7451e9bfa5823
VickyPlein <157711444+VickyPlein@users.noreply.github.com>
2024-02-07 11:38:09 +0200

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+//Single-line comment
+//Single-line comment
+
+/*
+multi line comment
+
+/*
+
+busimos temos: 
+-duomenu tipai:
+   -skaiciai
+   -tekstas
+   -logines reiksmes
+   -objektas
+-salygos (if)
+- if
+- ternary
+- switch
+-ciklai (for)
+-funkcijos
+-ivykiai (events listeners)
+
+*/
+console.log(123);
+console.log(123123);
